1aae29c10ce54ed1be490a2e60ed02ab1c0ebd1c
rikicop <ruperto1@protonmail.com>
2021-09-23 11:24:50 -0400

    Navbar & Transition

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,18 @@
 import { ArrowDropDown, Notifications, Search } from "@material-ui/icons"
+import { useState } from "react"
 import "./navbar.scss"
 
 const Navbar = () => {
+    const [isScrolled, setIsScrolled] = useState(false)
+
+    window.onscroll =() =>{
+        setIsScrolled(window.pageYOffset=== 0 ? false : true);
+        return () => (window.onscroll = null);
+    }
+
+    console.log(isScrolled);
     return (
-        <div className="navbar">
+        <div className={isScrolled ? "navbar scrolled": "navbar"}>
            <div className="container">
                 <div className="left">
                     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png" alt="netflix" />
